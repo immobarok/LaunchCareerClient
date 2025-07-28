82c557acb401260c3abad3eccaea4d969346fa66
immobarok <immobarokali2001@gmail.com>
2025-07-28 14:25:41 +0600

    firebase deploy update

--- src/utils/axiosSecure.js
+++ src/utils/axiosSecure.js
@@ -2,37 +2,37 @@ import useAuth from "../hooks/UseAuth";
 import axios from 'axios';
 import { useNavigate } from "react-router";
 const axiosSecure = axios.create({
-  baseURL: 'https://job-pilot-server-pf92.vercel.app',
+  baseURL: 'https://job-pilot-server-pf92.vercel.app/',
 })
 const useAxiosSecure = () => {
-    const { user, logOut } = useAuth();
-    const navigate = useNavigate();
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
 
-    axiosSecure.interceptors.request.use(config => {
-      config.headers.Authorization = `Bearer ${user.accessToken}`
-      return config;
-    }, error => {
-      return Promise.reject(error);
-    })
+  axiosSecure.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${user.accessToken}`
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  })
 
-    axiosSecure.interceptors.response.use(res => {
-      return res;
-    }, error => {
-      const status = error.status;
-      if (status === 403) {
-        navigate('/forbidden');
-      }
-      else if (status === 401) {
-        logOut()
-          .then(() => {
-            navigate('/login')
-          })
-          .catch(() => { })
-      }
+  axiosSecure.interceptors.response.use(res => {
+    return res;
+  }, error => {
+    const status = error.status;
+    if (status === 403) {
+      navigate('/forbidden');
+    }
+    else if (status === 401) {
+      logOut()
+        .then(() => {
+          navigate('/login')
+        })
+        .catch(() => { })
+    }
 
-      return Promise.reject(error);
-    })
-    return axiosSecure;
-  };
+    return Promise.reject(error);
+  })
+  return axiosSecure;
+};
 
-  export default useAxiosSecure;
+export default useAxiosSecure;
